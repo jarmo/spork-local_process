7b5cad10b492d94f75a0ee0ff7a4abb3168fcefe
Jarmo Pertman <jarmo.p@gmail.com>
2011-09-29 18:07:37 +0300

    add better tab completion

--- lib/spork/ext/tab_completion.rb
+++ lib/spork/ext/tab_completion.rb
@@ -5,7 +5,11 @@ module Spork
     class TabCompletion
       def self.init
         Readline.completion_append_character = nil
-        Readline.completion_proc = proc {|s| Dir[s + "*"].map {|m| File.directory?(m) ? m + "/" : m}}
+        Readline.completion_proc = lambda do |s|
+          Dir[s + "*"].map do |m|
+            File.directory?(m) && m !~ /\/$/ ? m + "/" : m
+          end
+        end
       end
     end
   end
